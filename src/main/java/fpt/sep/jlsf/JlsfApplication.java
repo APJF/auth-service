9df710ac21bb596e8979ff5c97a34096af3b49e2
finn <phuongandphan@gmail.com>
2025-04-15 22:31:57 +0700

    Initial commit

---
+++ src/main/java/fpt/sep/jlsf/JlsfApplication.java
@@ -0,0 +1,13 @@
+package fpt.sep.jlsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JlsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JlsfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fpt/sep/jlsf/JlsfApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.sep.jlsf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JlsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
